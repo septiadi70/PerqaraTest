65c66dfe7b868ff8cf34f8b670a846150ccca4ba
septiadi70 <septiadi70@gmail.com>
2023-08-15 16:54:35 +0700

    feat: Layout for list favorites page

--- PerqaraTest/UI/ListFavorites/ListFavoritesViewController.swift
+++ PerqaraTest/UI/ListFavorites/ListFavoritesViewController.swift
@@ -8,11 +8,49 @@
 import UIKit
 
 class ListFavoritesViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    struct K {
+        static let cellId = "CellId"
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationItem.title = "Favorite Games"
 
-        // Do any additional setup after loading the view.
+        configTable()
     }
 
+}
+
+// MARK: - Helpers
+
+extension ListFavoritesViewController {
+    private func configTable() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.rowHeight = 100.0
+        tableView.separatorStyle = .none
+        tableView.register(GameTableViewCell.nib(), forCellReuseIdentifier: K.cellId)
+    }
+}
+
+// MARK: - UITableViewDataSource, UITableViewDelegate
+
+extension ListFavoritesViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let aCell = tableView.dequeueReusableCell(withIdentifier: K.cellId, for: indexPath)
+        guard let cell = aCell as? GameTableViewCell else { return aCell }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
