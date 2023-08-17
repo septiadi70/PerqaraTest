4cccd20c742260753be87ee266dccf84dfbbb932
septiadi70 <septiadi70@gmail.com>
2023-08-17 07:11:35 +0700

    feat: Open detail game from list favorites page implementation

--- PerqaraTest/UI/ListFavorites/ListFavoritesViewController.swift
+++ PerqaraTest/UI/ListFavorites/ListFavoritesViewController.swift
@@ -107,5 +107,11 @@ extension ListFavoritesViewController: UITableViewDataSource, UITableViewDelegat
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+        
+        if let game = viewModel.getGame(index: indexPath.row) {
+            let detailViewController = Injection.provideDetailGameViewController(gameId: game.id)
+            detailViewController.hidesBottomBarWhenPushed = true
+            navigationController?.pushViewController(detailViewController, animated: true)
+        }
     }
 }
