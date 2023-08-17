ce11a0f249c49d3f61408a005cd16387084db505
septiadi70 <septiadi70@gmail.com>
2023-08-17 07:20:56 +0700

    feat: Add custom alert class

---
+++ PerqaraTest/Helper/Alert.swift
@@ -0,0 +1,31 @@
+//
+//  Alert.swift
+//  PerqaraTest
+//
+//  Created by Andi Septiadi on 17/08/23.
+//
+
+import UIKit
+
+struct Alert {
+    var title: String? = nil
+    var message: String? = nil
+    var actions: [UIAlertAction]
+    
+    func present(at controller: UIViewController) {
+        let alertController = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert
+        )
+        for action in actions {
+            alertController.addAction(action)
+        }
+        controller.present(alertController, animated: true)
+    }
+    
+    static func basicAlert(title: String?, message: String?, handler: ((UIAlertAction) -> Void)? = nil) -> Alert {
+        let action = UIAlertAction(title: "OK", style: .default, handler: handler)
+        return Alert(title: title, message: message, actions: [action])
+    }
+}
